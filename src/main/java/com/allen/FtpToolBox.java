508bf3e04e47b656bf985963d990878f8f5d4ad4
pouyang <ouyangpingfly@163.com>
2021-01-25 17:36:01 +0800

    2021-01-18 13:54:22

--- src/main/java/com/allen/FtpToolBox.java
+++ src/main/java/com/allen/FtpToolBox.java
@@ -19,24 +19,7 @@ public class FtpToolBox {
      */
      public static void  uploadFileOrDid(String srcFile,String destDir,String destIp,int port,String destUser,String destPwd) throws Exception {
          System.out.println("@@@ 上传开始...");
-         FtpUploadFileOrDir.uploadFileOrDid(srcFile, destDir, "10.10.55.88", 21, "mpsp", "mpsp4321");
+         FtpUploadFileOrDir.uploadFileOrDid(srcFile, destDir, destIp, port, destUser, destPwd);
          System.out.println("@@@ 上传结束...");
     }
-
-
-
-
-    public static void cups_pre() throws Exception {
-        String[] srcDirS = {"C:\\0000000\\cups.Front4_PDO00004301\\.build\\classes\\com\\umpay\\front3\\adapter\\AbstractAdapter.class",
-                "C:\\0000000\\cups.Front4_PDO00004301\\.build\\classes\\com\\umpay\\front3\\adapter\\UM_Pay.class",
-                "C:\\0000000\\cups.Front4_PDO00004301\\.build\\classes\\com\\umpay\\front3\\adapter\\UM_Cancel.class",
-                "C:\\0000000\\cups.Front4_PDO00004301\\.build\\classes\\com\\umpay\\front3\\adapter\\UM_Refund.class",
-                "C:\\0000000\\cups.Front4_PDO00004301\\.build\\classes\\com\\umpay\\front3\\adapter\\UM_Revoke.class"};
-        String destDir = "/usr/mpsp/netbank/cups.Front4-app1/classes/com/umpay/front3/adapter";
-        for (String s : srcDirS) {
-        }
-        JavaPpcSsh.rpcShell("  cd /usr/mpsp/netbank/cups.Front4-app1/bin; ./restart.sh");
-    }
-
-
 }
